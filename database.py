4bdce91ae338fa8ec15ffad23790b19969acdf38
PedroOlveira <pedro.oliveira153@fatec.sp.gov.br>
2025-06-30 17:20:34 -0300

    HOST Postgre

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="postgresql://root:0FPzFDYo8FLzXgXPLIEKcinuUjNIogVk@dpg-d1hci42dbo4c73a6nkhg-a.oregon-postgres.render.com/cadastro_logistico",
+        host="dpg-d1hci42dbo4c73a6nkhg-a.oregon-postgres.render.com",
         user="root",
         password="0FPzFDYo8FLzXgXPLIEKcinuUjNIogVk",
         dbname="cadastro_logistico",
